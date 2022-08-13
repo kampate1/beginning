e3001fbfe3099e1d7c3a6022c2d9e4825c32b570
kampate1 <89059162+kampate1@users.noreply.github.com>
2022-08-13 08:59:44 -0400

    Update romanToInt.py

--- romanToInt.py
+++ romanToInt.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: int
         """
         
+        # dictionary
         dic = {"I": 1, 
                "V": 5, 
                "X": 10, 
@@ -13,11 +14,16 @@ class Solution(object):
                "D": 500, 
                "M": 1000}
         
+        # number out put what roman number represents 
         num = 0
+        
+        #going through each character in roman number
         for i in range(len(s)):
+            # if last character in roman number OR the value is greater than the next number 
             if i==len(s)-1 or dic[s[i]] >= dic[s[i+1]]:
                 num += dic[s[i]] 
             else:
-                num -= dic[s[i]]\
-            
+                # when number is less then the next number it will be subtracted 
+                num -= dic[s[i]]
+        # return roman as an int
         return num
