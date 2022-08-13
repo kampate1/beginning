0151763c2d6645bb88ea4cb134cd4f864fa65f3e
kampate1 <89059162+kampate1@users.noreply.github.com>
2022-08-13 08:56:37 -0400

    Create romanToInt.py

---
+++ romanToInt.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        dic = {"I": 1, 
+               "V": 5, 
+               "X": 10, 
+               "L": 50, 
+               "C": 100, 
+               "D": 500, 
+               "M": 1000}
+        
+        num = 0
+        for i in range(len(s)):
+            if i==len(s)-1 or dic[s[i]] >= dic[s[i+1]]:
+                num += dic[s[i]] 
+            else:
+                num -= dic[s[i]]\
+            
+        return num
